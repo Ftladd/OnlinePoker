edf12ca5a63eec8be5fa4b5977615f4e587f6052
Ftladd <finlay.ladd@gmail.com>
2023-03-08 10:37:01 -0600

    added endpoints for user actions

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './config';
 import express, { Express } from 'express';
 import ip from 'ip';
 import { playMatch } from './controllers/game';
+import { registerUser, logIn, getAllUsers } from './controllers/UserController';
 
 const app: Express = express();
 const { PORT } = process.env;
@@ -10,7 +11,12 @@ playMatch();
 
 app.use(express.json());
 
-app.post('/api/playGame', playMatch);
+app.post('/api/users', registerUser);
+app.post('/api/logIn', logIn);
+app.get('api/users', getAllUsers);
+/* app.get('/api/gameStart/:userId', playMatch);
+app.post('/api/bet/:userId', placeBet);
+app.post('/api/playGame', playMatch); */
 
 app.listen(PORT, () => {
   console.log(`App is listening on port http://${ip.address()}:${PORT}`);
